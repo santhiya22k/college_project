72d0f31d6113b13fc8e529d9271ad907694ac323
Santhiya k <santhiyasri22k@gmail.com>
2026-01-27 18:10:41 +0530

    first commit

--- node-backend/src/routes/authroutes.ts
+++ node-backend/src/routes/authroutes.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { login, signup } from "../controllers/authController";
 
 const router = Router();
-
+router.get("/test", (req, res) => {
+  res.json({ message: "Auth route working ✅" });
+});
 router.post("/signup", signup);
 router.post("/login", login);
 
